93d6366ecbd3a674e11ec72aaaeec1a1fd093e4d
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-03-02 10:48:09 +0000

    Added different directions for the fade out

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Sliding-Carousel
 
-Can be downloaded using npm install sliding-carousel
+Can be downloaded using npm i sliding-carousel
 Use the container element class i.e ".container" as the first arguement, the second should be the leftarrow class/id such as ".leftarrow" or "#leftarrow" and the next arguement should be for the right arrow such as ".rightarrow" or "#rightarrow".
 
-=======
-Ideal div layers would be:
+# Ideal div layers would be:
 
-outer div, 
-arrow left,
-arrow right,
-carousel div,
-images * however many,
-close carousel div,
-close outer div,
+Outer div, <br/>
+Arrow left, <br/>
+Arrow right, <br/>
+Carousel div, <br/>
+Images * however many, <br/>
+Close carousel div, <br/>
+Close outer div, <br/>
+<br/>
+
+Obviously style how you wish from there
 
 If you have any issues please don't hesitate to let me know in github issues or on my email address hello@liampugh.co.uk
--- carousel.js
+++ carousel.js
@@ -5,21 +5,31 @@ const carouselSlider = function(container, leftArrow, rightArrow) {
 
     let index = 0;
 
-    function tweenIn (target) {
+    function tweenIn (target, direction) {
         const tl = gsap.timeline({ paused: true });
 
-        tl.to(target, { display: 'block', duration: 0.01})
-        tl.fromTo(target, { x: -100, duration: 0.5, opacity: 0 }, {opacity: 1, x: 0,})
+        if(direction === 'right') {
+            tl.to(target, { display: 'block', duration: 0.01})
+            tl.fromTo(target, { x: -100, duration: 0.5, opacity: 0 }, {opacity: 1, x: 0,})
+        } else if (direction === 'left') {
+            tl.to(target, { display: 'block', duration: 0.01})
+            tl.fromTo(target, { x: 100, duration: 0.5, opacity: 0 }, {opacity: 1, x: 0,})
+        }
 
         return tl
         
     }
 
-    function tweenOut (currentDisplayed) {
+    function tweenOut (currentDisplayed, direction) {
         const tl = gsap.timeline({ paused: true });
 
-        tl.to(currentDisplayed, {x: 100, opacity: 0, duration: 0.5})
-        tl.to(currentDisplayed, { display: 'none', duration: 0.01})
+        if(direction === 'right') {
+            tl.to(currentDisplayed, {x: 100, opacity: 0, duration: 0.5})
+            tl.to(currentDisplayed, { display: 'none', duration: 0.01})
+        } else if (direction === 'left') {
+            tl.to(currentDisplayed, {x: -100, opacity: 0, duration: 0.5})
+            tl.to(currentDisplayed, { display: 'none', duration: 0.01})
+        }
 
         return tl
 
@@ -38,21 +48,17 @@ const carouselSlider = function(container, leftArrow, rightArrow) {
         })
         imgs[index].style.display = 'block'
         
-
-        console.log(leftArrow)
-
         const arrowLeft = document.querySelector(leftArrow)
         const arrowRight = document.querySelector(rightArrow)
 
-
         arrowLeft.addEventListener('click', function(e) {
-            let animationLeftOut = tweenOut(imgs[index])
+            let animationLeftOut = tweenOut(imgs[index], 'left')
             animationLeftOut.play()
             index--
             if(index === -1) {
                 index = imgs.length - 1
             }
-            let animationLeft = tweenIn(imgs[index])
+            let animationLeft = tweenIn(imgs[index], 'left')
             setTimeout(function() {
                 animationLeft.play()
             }, 500)
@@ -62,13 +68,13 @@ const carouselSlider = function(container, leftArrow, rightArrow) {
 
         arrowRight.addEventListener('click', function(e) {
 
-            let animationRightOut = tweenOut(imgs[index])
+            let animationRightOut = tweenOut(imgs[index], 'right')
             animationRightOut.play()
             index++
             if(index === imgs.length) {
                 index = 0
             }
-            let animationRight = tweenIn(imgs[index])
+            let animationRight = tweenIn(imgs[index], 'right')
             setTimeout(function() {
                 animationRight.play()
             }, 500)
